e7e442ead998fd34527ac1dcf88c264f4d7b4797
IDerevyansky <igor.derevyansky@gmail.com>
2022-10-31 21:58:57 -0700

    -

--- src/App.js
+++ src/App.js
@@ -3,12 +3,54 @@ import './App.css';
 
 function App() {
 
-  let abc = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*?><}{';
-  var random = () => { return Math.floor(Math.random() * 75)};
 
-  
-  let link, key = [];
-  let generationLink = () => {}
+  let genLink = (q) => {
+
+    let abc = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*?><}{';
+    var random = () => { return Math.floor(Math.random() * abc.length)};
+
+    let getRandomInt = (min, max) => {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+
+    let i = 0;
+    let key = [];
+    let link = [];
+
+
+    switch (q) {
+
+      // ---Generation link start---
+      case 'link':
+        while (i <= getRandomInt(40,80)) {
+          link[i] = abc[random()]
+          i++
+        }
+        return link.join('');
+      break;
+      // ---Generation link end---
+
+      // ---Generation key start---
+      case 'key':
+        while (i <= getRandomInt(8,28)) {
+          key[i] = abc[random()]
+          i++
+        }
+        return key.join('');
+      break;
+      // ---Generation key end---
+
+      default:
+      break;
+    }
+
+  }
+
+ console.log( genLink('link') );
+
 
   return (
 
@@ -17,6 +59,9 @@ function App() {
     </>
 
   );
+
+
+
 }
 
 export default App;
